736fadd03a9ee53a190a13578dc3e95f08c675fa
Andreas Galanis <agalanis7@gmail.com>
2019-01-18 11:16:14 -0500

    content added in About Me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,13 +10,27 @@ export default class AboutMe extends Component{
       <h1>About Me</h1>
 
       <picture>
-      <source media="(min-width: 300px)" srcset="https://pbs.twimg.com/profile_images/739109952212467713/Vr_n84CJ.jpg"></source>  
-      <source media="(min-width: 500px)" srcset="http://all4desktop.com/data_images/original/4246355-happy.jpg"></source>
-      <img src="https://pbs.twimg.com/profile_images/739109952212467713/Vr_n84CJ.jpg" alt="my profile picture"/>
+      <source media="(min-width: 300px)" srcset="https://res.cloudinary.com/dre/image/upload/v1547748233/me.jpg"></source>  
+      <source media="(min-width: 500px)" srcset="https://res.cloudinary.com/dre/image/upload/v1547748233/me.jpg"></source>
+      <img src="https://res.cloudinary.com/dre/image/upload/v1547748233/me.jpg" alt="my profile picture"/>
       </picture>
 
       <div>
-        <p>This is the AboutMe section.</p>
+        <p>I graduated from The University of Maryland, College Park with a BS in Finance in 2006.  
+            After that I went to work for a mortgage company as a staff accountant and office manager.  
+            When the housing market crashed in the late 2000's I went to work for my family's construction company,
+            Daystar Contracting, LLC, as a construction project manager.
+            I got my first taste of coding at Daystar Contracting, LLC, where I developed a website for the company.
+            I really enjoyed the experience, and pursued coding through an online course at HackReactor, a San Francisco based coding
+            school.  I learned HTML, CSS, and JavaScript fundamentals at HackReactor.  Again, I really enjoyed my coding experience, and 
+            decided to continue developing my coding skills.  In August 2018 I moved to Miami, FL to attend the Full Stack Web Development
+            Program at Wyncode Academy.  At Wyncode, I continued building my HTML, CSS, and Javascript skills, as well as 
+            learning React.js, and Ruby on Rails.  I had a wonderful experience at Wyncode, and my time there helped me to realize that coding
+            is what I want to  do  for the rest of my life.
+            After Wyncode I got an internship at Miami Dev Shop, assisting senior developers on production apps.  I got some exposure to Node.js, and Hapi.js while I was there.
+            Now I am ready for a full-time position as a full stack web developer!    
+
+        </p>
       </div>
 
     </main>
